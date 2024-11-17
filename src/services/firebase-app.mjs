88d8708098cf22b38e2134b38596368eed6df6af
Zarin Loosli <zarinloosli@gmail.com>
2024-11-16 19:18:24 -0700

    Delete services folder

--- src/services/firebase-app.mjs
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from "../libs/firebase/9.7.0/firebase-app.js";
-
-const firebaseConfig = %firebase-config%;
-
-// Initialize Firebase
-/** The Firebase app */
-export default initializeApp(firebaseConfig);
