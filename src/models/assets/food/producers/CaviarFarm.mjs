9b4ffb402cb8f240aead4037754fb9ac67ddce10
Zarin Loosli <zarinloosli@gmail.com>
2024-11-23 16:13:44 -0700

    Update CaviarFarm to use static members

--- src/models/assets/food/producers/CaviarFarm.mjs
+++ src/models/assets/food/producers/CaviarFarm.mjs
@@ -4,14 +4,10 @@ import { Asset } from "../../Asset.mjs";
 import * as Resources from "../../../Resources.mjs";
 import { register } from "../../../game-state/GlobalAssetDirectory.mjs";
 
-export class RamenFarm extends Asset {
-  constructor() {
-    super({
-      prettyName: "Ramen Farm",
-      produces: [{ resource: Resources.FOOD, amount: 10 }],
-      consumes: [{ resource: Resources.MONEY, amount: 3 }],
-    });
-  }
+export class CaviarFarm extends Asset {
+  static prettyName = "Ramen Farm";
+  static produces = [{ resource: Resources.FOOD, amount: 10 }];
+  static consumes = [{ resource: Resources.MONEY, amount: 3 }];
 }
 
-register(new RamenFarm());
+register(new CaviarFarm());
